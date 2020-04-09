19060ef223e40d957f50eeae88e80f8176b60885
Kanzaki404 <alexander.risteski23@gmail.com>
2020-04-09 14:43:16 +0200

    devBranch

---
+++ mii/src/components/list-component.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>This is the list component</h1>
+        <editComponent></editComponent>
+    </div>
+</template>
+
+<script>
+import editComponent from './edit-component'   // figure it out for edit component to only open when edit button pressed maybe routing
+export default {
+
+  name: 'listComponent',
+  components: {
+    editComponent
+  }
+  
+}
+</script>
--- mii/src/views/Home.vue
+++ mii/src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <listComponent></listComponent>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import listComponent from '../components/list-component'
 export default {
   name: 'Home',
   components: {
+    listComponent
   }
 }
 </script>
